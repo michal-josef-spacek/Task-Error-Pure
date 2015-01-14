f541454c3ee7d543fd575c918b624fe92233eed8
Michal Špaček <tupinek@gmail.com>
2015-01-14 19:05:07 +0100

    First version.

---
+++ Pure.pm
@@ -0,0 +1,57 @@
+package Task::Error::Pure;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Error::Pure - Install the Error::Pure modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Error::Pure
+
+=head1 SEE ALSO
+
+L<Error::Pure>,
+L<Error::Pure::Always>,
+L<Error::Pure::HTTP>,
+L<Error::Pure::HTTP::JSON>,
+L<Error::Pure::JSON>,
+L<Error::Pure::NoDie>,
+L<Error::Pure::Output::JSON>,
+L<Error::Pure::Output::Text>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Taks-Error-Pure>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
